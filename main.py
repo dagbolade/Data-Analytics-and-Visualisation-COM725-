0a26984ba7e61d10f8f64c272321cc280a78e3ef
David Damilola <dagbolade72@gmail.com>
2024-01-29 20:05:50 +0000

    documenting com 725 lectures and notes

---
+++ main.py
@@ -0,0 +1,10 @@
+# data structures in Data Science
+
+# python data structures
+# vector
+
+# vector as a row using np.array
+import numpy as np
+
+vector_row = np.array([1, 2, 3])
+print(vector_row)
